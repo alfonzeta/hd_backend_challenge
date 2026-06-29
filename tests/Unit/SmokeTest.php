a10b771d90301f74994c20af56cf10836ea61af9
alfonzeta <alfonso.za94@gmail.com>
2026-06-29 18:46:28 +0200

    test: add PHPUnit configuration and smoke test

---
+++ tests/Unit/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testSmoke(): void
+    {
+        self::assertTrue(true);
+    }
+}
